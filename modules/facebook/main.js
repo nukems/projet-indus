a1b00d3ddca5dd6c7d531ae2c00c8268ffb8e907
kepsylis <kevin.yot@gmail.com>
2014-04-29 16:51:08 +0200

    fb api

--- modules/facebook/main.js
+++ modules/facebook/main.js
@@ -1,5 +1,6 @@
 var facebook = require('./facebook.js');
 var ConfigChecker = require('./../../back/ConfigChecker.js');
+var FB = require('./../../front/node_modules/fb');
 
 var accessToken = "CAADTa5xIkQUBACN5WetnSYdjAjn2Ddn1ZBkQ0gwnMzCY67hk4JlHUk4fQDvHbUlAR5mUlRL2SBIQKS6JsC25zY6sOzGHYbZAoUrBZBNAUgHrzFs3IqwdXCMzE1kwQtU4pp6y3agL2e6lZCBWHHgwAdjtXqdZCK0YRZCFXnQj2gR7Inz4WNPZBZA9oSjbZBt3iGWsZD";
 
@@ -7,12 +8,26 @@ function execute(callback) {
 	console.log("Execution du module facebook");
 
 	ConfigChecker.get("facebook", function(linkFacebook){
+	
+		FB.api('oauth/access_token', {
+					
+			client_id: '232459136962821',
+			client_secret: '6e27e1d43a2d3132146ff4ab9ebe72ab',
+			grant_type: 'client_credentials'
+			
+		}, function (resu) {
+			
+			//var accessToken = resu.access_token;
+			FB.setAccessToken(accessToken);
+			
+			for(var i = 0; i < linkFacebook.length; i++)
+			{
+				doInfoPageFacebookRequest(linkFacebook, i);
+				doPostFacebookRequest(linkFacebook, i);
+			}
+			
+		});
 		
-		for(var i = 0; i < linkFacebook.length; i++)
-		{			
-			doInfoPageFacebookRequest(linkFacebook, i);			
-			doPostFacebookRequest(linkFacebook, i);
-		}
 	});
 	
 	callback();	
@@ -23,11 +38,9 @@ function execute(callback) {
  */
 function doInfoPageFacebookRequest(linkFacebook, index) {
 	
-	facebook.get(('/' + linkFacebook[index].fields.pageName + '?fields=likes,talking_about_count'), function(response) {
+	FB.api(linkFacebook[index].fields.pageName, { fields : ['likes', 'talking_about_count'] }, function(response) {
 		if(response)
 		{
-			response = JSON.parse(response);
-			
 			var dataFBPage = {
 				"connector_name" : "facebook",
 				"type" : "info_page",
@@ -53,14 +66,15 @@ function doInfoPageFacebookRequest(linkFacebook, index) {
  */
 function doPostFacebookRequest(linkFacebook, index) {
 	
-	facebook.get(('/' + linkFacebook[index].fields.pageName + '/statuses/?fields=likes.limit(1).summary(true),comments.limit(1).summary(true),message,updated_time&&access_token=' + accessToken), function(response) {
+	FB.api((linkFacebook[index].fields.pageName + '/statuses/'), { fields : ['likes.limit(1).summary(true)', 'comments.limit(1).summary(true)', 'message', 'updated_time']}, function(response) {
 		
 		if(response)
-		{
-			response = JSON.parse(response);
-			
+		{	
+			console.log(response);	
 			for(var j = 0; j < response.data.length; j++)
 			{		
+				//console.log(response.data[j].comments.paging);
+				
 				var dataFBPost = {
 					"connector_name" : "facebook",
 					"type" : "post",
