78116c7bb0c637c929eb25ce15711a209b280af2
FlorianGomez <floriangomez11@gmail.com>
2014-04-29 16:04:22 +0200

    init.js

--- back/core/Routes.controller.js
+++ back/core/Routes.controller.js
@@ -1,29 +1,22 @@
 function Core_Routes() {
 
+	var self = this;
+
 	this.url = null;
 
 	/**
 	*	Recuperation du module et execution du script principal
 	*/
 	this.exec = function() {
-		if (this.getUrl() == null) {
-			console.log("Requete incorrecte : il faut renseigner une url");
-			res.end(-1);
-		} else {
-			urlParts = this.getUrl().split('/');
-			if (urlParts.length == 1) {
-				console.log("Requete incorrecte : il faut indiquer le module a executer");
-				res.end(-1);
-			} 
-			var moduleName = urlParts[1];
+		var moduleName = self.getUrl();
 
-			if (moduleName != 'favicon.ico') {
-				var module = require("../../modules/" + moduleName + "/main.js");
-				module.execute(function() {
-					console.log('Execution du module '  + moduleName + ' avec succes');
-					res.end(0);
-				});
-			}
+		try {
+			var module = require("../../modules/" + moduleName + "/main.js");
+			module.execute(function() {
+				console.log('Execution du module '  + moduleName + ' avec succes');
+			});
+		} catch (e) {
+			console.log("ERROR EXECUTION MODULE : " + e);
 		}
 	}
 
---
+++ back/init.js
@@ -0,0 +1,31 @@
+/**
+*	Execution du code pour le cron
+*/
+function init() {
+	//initialisation de l'autoload
+	var i = require('./core/Instances.controller.js').controller;
+	global.InstancesController = new i();
+
+	//connexion a la base de donnees
+	Database = InstancesController.getInstance('Core_Database');
+	Database.connect(function() {
+		//initialisation de la gestion des routes
+		RoutesController = InstancesController.getInstance('Core_Routes');
+		RoutesController.setUrl(args[2]);
+
+		//execution de code pour la route
+		//donc du module
+		RoutesController.exec();
+	});
+}
+
+global.env = require("../lib/config.js").getConfig();
+global.args = [];
+process.argv.forEach(function (val, index, array) {
+  args.push(val);
+});
+if (args.length == 2) {
+	console.log("ERROR : no module defined");
+} else {
+	init();
+}
